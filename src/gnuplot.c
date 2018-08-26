32f338fdfb0309414a462a6df2ea10721d8d5db3
Durfan <31577607+Durfan@users.noreply.github.com>
2018-08-26 19:49:07 -0300

    ugd svg

--- src/gnuplot.c
+++ src/gnuplot.c
@@ -2,7 +2,6 @@
 
 void plotGraph(conjunto_t *CJT) {
     FILE *temp = fopen("data.temp", "w");
-    FILE *gnuplot = popen("gnuplot", "w");
 
     node_t *read = CJT->head;
     while (read != NULL) {
@@ -15,12 +14,15 @@ void plotGraph(conjunto_t *CJT) {
     }
     fclose(temp);
 
+    FILE *gnuplot = popen("gnuplot", "w");
+
     fprintf(gnuplot, "set terminal svg enhanced mouse size 1280,1280\n");
     if (!CJT->ncoords) fprintf(gnuplot, "set output 'output.svg'\n");
     else fprintf(gnuplot, "set output 'input.svg'\n");
-    fprintf(gnuplot, "set title \"Debug\"\n");
+    fprintf(gnuplot, "set title \"Solucao da entrada\"\n");
+    fprintf(gnuplot, "set style line 12 lc rgb '#808080' lt 0 lw 1\n");
+    fprintf(gnuplot, "set grid back ls 12\n");
     fprintf(gnuplot, "plot 'data.temp' w lp\n");
-
 
     fflush(gnuplot);
     pclose(gnuplot);
